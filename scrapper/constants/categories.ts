3db672cab34b9dcfb726f9864e58b848acb3ffc8
JoseLuria <jos02-log18@hotmail.com>
2023-02-25 17:13:19 -0600

    feat: :sparkles: added categories selectors

---
+++ scrapper/constants/categories.ts
@@ -0,0 +1,18 @@
+export const CATEGORIES_URLS = [
+  {
+    category: 'celulares',
+    url: 'https://www.coppel.com/ofertas-celulares'
+  },
+  {
+    category: 'electronica',
+    url: 'https://www.coppel.com/ofertas-electronica'
+  },
+  {
+    category: 'videojuegos',
+    url: 'https://www.coppel.com/ofertas-consolas-y-videojuegos'
+  }
+]
+
+export const CATEGORIES_ITEM = '.product_listing_container .product'
+
+export const CTEGORIES_ITEM_URL = '.product_name a'
