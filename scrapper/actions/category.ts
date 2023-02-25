fc2afc519c04c6587f68dbc288789de67564377f
JoseLuria <jos02-log18@hotmail.com>
2023-02-25 17:46:39 -0600

    feat: :sparkles: created category actions

---
+++ scrapper/actions/category.ts
@@ -0,0 +1,30 @@
+import { scrape } from '../utils'
+import { CATEGORIES_URLS, CATEGORIES_ITEM, CTEGORIES_ITEM_URL } from '../constants'
+
+interface Category {
+  url: string
+  category: string
+}
+
+export const getCategoryProducts = async () => {
+  const productsUrls: Category[] = []
+
+  for (const { url, category } of CATEGORIES_URLS) {
+    const $ = await scrape(url)
+
+    const hrefs = $(CATEGORIES_ITEM).map((_, el) => {
+      const href = $(el).find(CTEGORIES_ITEM_URL).attr('href')
+      return { url: href, category }
+    })
+
+    const categoryProducts = [...hrefs]
+
+    categoryProducts.forEach(({ url, category }) => {
+      if (url) {
+        productsUrls.push({ url, category })
+      }
+    })
+  }
+
+  return productsUrls
+}
