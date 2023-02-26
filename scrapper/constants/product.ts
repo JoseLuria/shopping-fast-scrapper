1cfefe3ad213ba1e396a51829e5ec0a48da73ebe
JoseLuria <jos02-log18@hotmail.com>
2023-02-26 12:24:36 -0600

    refactor: :art: separate products selectors in constants

---
+++ scrapper/constants/product.ts
@@ -0,0 +1,61 @@
+export const PRODUCT_SELECTORS = [
+  {
+    name: 'name',
+    selector: 'h1',
+    attr: null,
+    iterator: null
+  },
+  {
+    name: 'priceUnique',
+    selector: '.group_price .unique_price',
+    attr: null,
+    iterator: null
+  },
+  {
+    name: 'priceOld',
+    selector: '.group_price .old_price',
+    attr: null,
+    iterator: null
+  },
+  {
+    name: 'discount',
+    selector: '.tam_normal .discountRibbon',
+    attr: null,
+    iterator: null
+  },
+  {
+    name: 'description',
+    selector: '.productContainerDescription .content #longDescriptionWidgetId div',
+    attr: null,
+    iterator: null
+  },
+  {
+    name: 'imageContainer',
+    selector: '.image_container a',
+    attr: 'href',
+    iterator: null
+  },
+  {
+    name: 'imagesElements',
+    selector: '.other_views ul li a',
+    attr: null,
+    iterator: [{ name: null, find: '', attr: 'href' }]
+  },
+  {
+    name: 'properties',
+    selector: '#descriptiveAttributesWidgetId ul li',
+    attr: null,
+    iterator: [
+      { name: 'name', find: 'span:first-child', attr: null },
+      { name: 'value', find: 'span:last-child', attr: null }
+    ]
+  }
+]
+
+export const PRODUCTS_REMOVE_PROPERTIES = [
+  'imageContainer',
+  'imagesElements',
+  'priceOld',
+  'priceUnique',
+  'propertiesElements'
+]
