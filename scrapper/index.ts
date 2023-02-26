17afa5569a9e5ee6d75c8ea726f6e62436428a22
JoseLuria <jos02-log18@hotmail.com>
2023-02-26 13:46:10 -0600

    feat: :sparkles: added scrape products and save to json

--- scrapper/index.ts
+++ scrapper/index.ts
@@ -1,7 +1,22 @@
-import { getCategoryProducts } from './actions'
+import { getCategoryProducts, getIndividualProduct } from './actions'
+import { Product } from './types'
+import { productSchema } from './validations'
 import { saveJSON } from './utils'
 
 ;(async () => {
+  const products: Product[] = []
+
   const categoryProducts = await getCategoryProducts()
-  await saveJSON(JSON.stringify(categoryProducts))
+
+  for await (const invidualProduct of categoryProducts) {
+    const productData = await getIndividualProduct(invidualProduct)
+    const validate = productSchema.safeParse(productData)
+    if (validate.success) {
+      const { data } = validate
+      products.push(data)
+      console.log(`Scrapped product ${data.name}`)
+    }
+  }
+
+  await saveJSON(JSON.stringify(products))
 })()
