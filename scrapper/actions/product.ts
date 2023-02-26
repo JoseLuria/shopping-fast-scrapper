963b1b6e2478849e322f637c77e4e69ea9abc8da
JoseLuria <jos02-log18@hotmail.com>
2023-02-26 13:28:02 -0600

    feat: :sparkles: add download images on product scrapping

--- scrapper/actions/product.ts
+++ scrapper/actions/product.ts
@@ -1,6 +1,7 @@
+import { v4 as uuid } from 'uuid'
 import { Category } from '../types'
 import { PRODUCT_SELECTORS, PRODUCTS_REMOVE_PROPERTIES } from '../constants'
-import { scrape, createSlug, formatDiscount, createStock, formatNumber } from '../utils'
+import { scrape, createSlug, formatDiscount, createStock, formatNumber, saveImage } from '../utils'
 
 export const getIndividualProduct = async ({ url, category }: Category) => {
   const $ = await scrape(url)
@@ -16,25 +17,46 @@ export const getIndividualProduct = async ({ url, category }: Category) => {
       const values: any[] = []
 
       el.each((_, it) => {
+        const data: any = {}
+
         iterator.forEach(({ attr, find, name }) => {
           const el = $(it)
           const iteratorValue = attr ? el.attr(attr) : el.find(find).text().trim()
-          values.push({ name, value: iteratorValue })
+          data[name] = iteratorValue
         })
+
+        values.push(data)
       })
 
-      data[name] = iterator.length > 1 ? values : values.map(({ value }: any) => value)
+      data[name] = iterator.length > 1 ? values : values.map(({ image }: any) => image)
     }
   })
 
   const { imagesElements, imageContainer, priceUnique, priceOld, discount } = data
 
   data.discount = discount ? formatDiscount(discount) : null
-  data.images = [imageContainer, ...imagesElements]
   data.price = priceUnique || priceOld
   data.slug = createSlug(data.name)
   data.stock = createStock()
   data.price = formatNumber(data.price)
+
+  const images: string[] = []
+
+  const productImages = [imageContainer, ...imagesElements]
+
+  for await (const image of productImages) {
+    const id = uuid()
+
+    try {
+      const imagePath = await saveImage(image, `${id}.jpg`)
+      images.push(imagePath)
+      console.log(`Image ${imagePath} saved`)
+    } catch (error) {
+      console.log(`Image ${image} from product ${data.name} not found`)
+    }
+  }
+
+  data.images = images
 
   PRODUCTS_REMOVE_PROPERTIES.forEach((value) => {
     delete data[value]
